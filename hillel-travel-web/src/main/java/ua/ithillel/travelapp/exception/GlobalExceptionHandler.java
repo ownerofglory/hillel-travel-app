9a4821b4e0651ff830d653297e09e8b98f65e6ec
Zasoba <vladyslav.v.zasoba@mercedes-benz.com>
2023-09-05 21:53:26 +0200

    Add InconsistencyException handler

--- hillel-travel-web/src/main/java/ua/ithillel/travelapp/exception/GlobalExceptionHandler.java
+++ hillel-travel-web/src/main/java/ua/ithillel/travelapp/exception/GlobalExceptionHandler.java
@@ -16,6 +16,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDTO);
     }
 
+    @ExceptionHandler(InconsistencyException.class)
+    public ResponseEntity<ErrorDTO> handleInconsistencyException(InconsistencyException e) {
+        ErrorDTO errorDTO = new ErrorDTO();
+        errorDTO.setMessage(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDTO);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorDTO> handleAnyException(Exception e) {
         ErrorDTO errorDTO = new ErrorDTO();
